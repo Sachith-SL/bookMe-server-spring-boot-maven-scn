46794ccfe9af2ca722398e1abae5adf89aed900d
Sachith-SL <itsmesachith@gmail.com>
2024-07-28 13:26:15 +0530

    Initial Commit

---
+++ src/test/java/com/sachith/server/BookMeServerSpringBootMavenScnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachith.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMeServerSpringBootMavenScnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
